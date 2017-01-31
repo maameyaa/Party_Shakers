412ee868c1e3ed631ddca88e2ef713879c13b082
tobelum <tohbelle@gmail.com>
2017-01-31 16:40:44 +0000

    Search function works
    Items can be searched for in the searchbox using either name or
    category.

---
+++ parts.php
@@ -0,0 +1,41 @@
+<?php
+     include_once("adb.php");
+     
+     /**
+	*Variables used in all the functions for Patients
+	*@param int pno
+	*@param string pname
+	*@param int qoh
+	*@param int price
+	*@param int olevel
+	*@param enum category
+	*/
+
+class parts extends adb{
+     function parts(){
+
+     }
+
+     function newPart($pname='none',$qoh='none',$price='none',$olevel='none',$category='none'){
+    
+          $strQuery = "insert into parts SET pname = '$pname',qoh = '$qoh',price = '$price',olevel = '$olevel',category = '$category'";
+          // echo $strQuery;
+          return $this->query ($strQuery);
+     }
+
+     function getPart($filter=false){
+          $strQuery="select pname, price, category from parts";
+          if($filter!=false){
+            $strQuery=$strQuery . " where pname like '%$filter%' or category like '%$filter%'";
+          }
+          return $this->query($strQuery);
+     }
+
+     function editPart($pno,$pname,$qoh,$price,$olevel,$category){
+          $strQuery = "update parts SET pname = '$pname',qoh = '$qoh',price = '$price',olevel = '$olevel',category = '$category' where 
+                      pno = $pno";
+          return $this->query($strQuery);
+     }
+
+
+   }
